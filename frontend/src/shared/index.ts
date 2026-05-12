780718f8dffcc18053a211f436965f3839f970a8
Pooja Kulkarni <pooja@opencraft.com>
2026-05-12 10:18:36 -0400

    feat: Add runtime theme support

--- frontend/src/shared/index.ts
+++ frontend/src/shared/index.ts
@@ -1,3 +1,1 @@
-import "@openedx/paragon/dist/core.min.css";
-import "@openedx/paragon/dist/light.min.css";
 import "./styles.css";
--- frontend/src/shared/mountApp.tsx
+++ frontend/src/shared/mountApp.tsx
@@ -38,11 +38,73 @@ function resolveMountNode(elementLike: XBlockElementLike, selector?: string) {
   return nextMountNode;
 }
 
-export function mountReactTree(
+interface ThemeUrls {
+  core: {
+    urls: {
+      default: string;
+      brandOverride: string;
+    };
+  };
+  default: {
+    light: string;
+    [key: string]: string;
+  };
+  variants: {
+    [key: string]: {
+      urls: {
+        brandOverride: string;
+      };
+    };
+  };
+}
+
+async function getThemes() {
+  let themeUrls: ThemeUrls | Record<string, never> = {};
+  try {
+    const response = await fetch("/api/mfe_config/v1");
+    const mfeConfig = await response.json();
+    themeUrls = mfeConfig.PARAGON_THEME_URLS;
+  } catch (error) {
+    console.error("Failed to fetch theme URLs:", error);
+  }
+  const variant = themeUrls?.["default"]?.["light"];
+  const theme: string[] = [
+    "https://cdn.jsdelivr.net/npm/@openedx/paragon@23/dist/light.min.css",
+    themeUrls?.["variants"]?.[variant]?.["urls"]?.["brandOverride"],
+  ].filter(Boolean);
+  const core: string[] = [
+    "https://cdn.jsdelivr.net/npm/@openedx/paragon@23/dist/core.min.css",
+    themeUrls?.["core"]?.["urls"]?.["brandOverride"],
+  ].filter(Boolean);
+
+  return { core, theme };
+}
+
+export async function mountReactTree(
   element: XBlockElementLike,
   reactElement: React.ReactElement,
   selector?: string,
 ) {
+  const { core, theme } = await getThemes();
+
+  core.forEach((url) => {
+    if (!document.head.querySelector(`link[href="${url}"]`)) {
+      const link = document.createElement("link");
+      link.rel = "stylesheet";
+      link.href = url;
+      document.head.appendChild(link);
+    }
+  });
+
+  theme.forEach((url) => {
+    if (!document.head.querySelector(`link[href="${url}"]`)) {
+      const link = document.createElement("link");
+      link.rel = "stylesheet";
+      link.href = url;
+      document.head.appendChild(link);
+    }
+  });
+
   const mountNode = resolveMountNode(element, selector);
 
   const root = createRoot(mountNode);
